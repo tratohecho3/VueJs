b27fa7801e6414dd3e6487d1a18a42f1f50660e7
Cesar <tratohecho3@gmail.com>
2018-06-19 18:56:44 -0400

    using the last fm api

---
+++ platzimusic/src/App.vue
@@ -0,0 +1,51 @@
+<template lang="pug">
+#app
+  img(src='./assets/logo.png')
+  h1 PlatziMusic
+  ul 
+    li(v-for="artist in artists") {{artist.name}}
+
+</template>
+
+<script>
+import getArtists from './api'
+export default {
+  name: 'app',
+  data () {
+    return {
+      artists:[]
+    }
+  },
+  mounted: function(){
+    const self = this
+    getArtists()
+      .then( function(artists){
+        self.artists = artists
+      })
+  }
+}
+</script>
+
+<style lang="stylus">
+#app
+  font-family 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+  margin-top 60px
+
+h1, h2
+  font-weight normal
+
+ul
+  list-style-type none
+  padding 0
+
+li
+  display inline-block
+  margin 0 10px
+
+a
+  color #42b983
+</style>
---
+++ platzimusic/src/api/config.js
@@ -0,0 +1,7 @@
+
+export default {
+    appname: 'platzimusic',
+    apiKey: '8e42f87c817ffa1c26b2301cc3e0d981',
+    secret: '5477f242fbc3689b1bf2088431d0679d',
+    registeredTo: 'ayacayu'
+}
